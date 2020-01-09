d53484d9aff29feee07ed647a4b7ba5ebe60f6d4
carlos.molina <carlos.molina@ex-cle.com>
2020-01-09 14:41:27 -0400

    update dashboard
    new service gas price
    update accounts 
    update tabs
    update import
    update global
    add svg

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { HeliosServiceService } from '../../services/helios-service.service';
 import { LoadingController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-home',
@@ -13,11 +15,18 @@ export class HomePage implements OnInit {
   constructor( 
     private storage: Storage,
     private heliosService: HeliosServiceService,
-    private loadingController: LoadingController
-    ) { }
+    private loadingController: LoadingController,
+    private route: ActivatedRoute
+    ) {
+      route.params.subscribe(val => {
+        this.inicialize();
+      });
+    }
 
-  wallet: string;
+  wallets: [];
   balance: any;
+  gasPrice: number;
+  today: any;
   slideOpts = {
     slidesPerView: 2,
     initialSlide: 0,
@@ -25,21 +34,25 @@ export class HomePage implements OnInit {
   };
 
   ngOnInit() {
-    // Or to get a key/value pair
-    this.storage.get('wallet').then(async (wallet) => {
-      if (wallet != null) {
+    //this.inicialize();
+  }
+
+  inicialize() {
+    this.today = moment();
+    this.storage.get('wallet').then(async (wallets) => {
+      if (wallets != null) {
         const loading = await this.loadingController.create({
           message: 'Please wait...',
           translucent: true,
           cssClass: 'custom-class custom-loading'
         });
         await loading.present();
-        this.wallet = wallet;
+        this.wallets = wallets;
         this.balance = await this.heliosService.getBalance('0x4A1383744eED3DBE37B7A0870b15FeA3cE319A66');
         //this.heliosService.getAllTransactions('0x4A1383744eED3DBE37B7A0870b15FeA3cE319A66', startDate, endDate);
         await loading.dismiss();
       }
+      this.gasPrice = await this.heliosService.getGasPrice();
     });
   }
-
 }
