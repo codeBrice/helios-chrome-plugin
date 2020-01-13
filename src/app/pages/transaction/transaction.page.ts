c829ded77ba041b23b5e0eced2bb8f5c3bd8adee
jesus.briceno <jesus.bricenio@ex-cle.com>
2020-01-13 13:33:50 -0400

    Ajustes reconexión a nodos y transacciones

--- src/app/pages/transaction/transaction.page.ts
+++ src/app/pages/transaction/transaction.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { Storage } from '@ionic/storage';
+import { HeliosServiceService } from '../../services/helios-service.service';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-transaction',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransactionPage implements OnInit {
 
-  constructor() { }
+  transactions: any;
+  constructor(
+    private heliosService: HeliosServiceService,
+    private storage: Storage,
+    private loadingController: LoadingController
+    ) { }
 
   ngOnInit() {
+    let startDate = moment().utc().subtract(3, 'months').valueOf();
+    let endDate = moment().utc().valueOf();
+
+    this.storage.get('wallet').then(async (wallets) => {
+      const loading = await this.loadingController.create({
+        message: 'Please wait...',
+        translucent: true,
+        cssClass: 'custom-class custom-loading'
+      });
+      await loading.present();
+      for (const wallet of wallets) {
+          const tx = await this.heliosService.getAllTransactions( wallet , startDate , endDate, null, null);
+          this.transactions = tx;
+          console.log ( 'getTransactions', tx ); 
+      }
+      await loading.dismiss();
+    });
   }
 
 }
