e32ebc452ea5d9eca6b34f4ca3ffd05431ebf928
jesus.briceno <jesus.bricenio@ex-cle.com>
2020-01-29 15:41:31 -0400

    Se ajusta la pantalla de transacciones y agregan librerias para android

--- src/app/pages/transaction/transaction.page.ts
+++ src/app/pages/transaction/transaction.page.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as moment from 'moment';
 import { Storage } from '@ionic/storage';
 import { HeliosServiceService } from '../../services/helios-service.service';
-import { LoadingController, ModalController} from '@ionic/angular';
+import { LoadingController, ModalController } from '@ionic/angular';
 import { TransactionDetailModalPage } from '../transactionDetail/transaction-detail-modal/transaction-detail-modal.page';
 
 @Component({
@@ -11,13 +11,14 @@ import { TransactionDetailModalPage } from '../transactionDetail/transaction-det
   styleUrls: ['./transaction.page.scss'],
 })
 export class TransactionPage implements OnInit {
-
-  transactions: any;
+  fromTx: any;
+  toTx: any;
+  transactions: any[] = [];
   constructor(
     private heliosService: HeliosServiceService,
     private storage: Storage,
     private loadingController: LoadingController,
-    private modalController: ModalController
+    private modalController: ModalController,
     ) { }
 
   ngOnInit() {
@@ -31,11 +32,15 @@ export class TransactionPage implements OnInit {
         cssClass: 'custom-class custom-loading'
       });
       await loading.present();
+      this.fromTx = 0;
+      this.toTx = 10;
       for (const wallet of wallets) {
-          const tx = await this.heliosService.getAllTransactions( wallet , startDate , endDate, null, null);
-          this.transactions = tx;
-          this.transactions.map( data => data.timestamp = moment.unix(data.timestamp));
-          console.log ( 'getTransactions', tx ); 
+          const tx = await this.heliosService.getAllTransactions( wallet , startDate , endDate, this.fromTx, this.toTx);
+          this.transactions = tx.map( data => {
+            data.timestamp = moment.unix(data.timestamp);
+            return data;
+          });
+          console.log ( 'getTransactions', tx );
       }
       await loading.dismiss();
     });
@@ -57,4 +62,21 @@ export class TransactionPage implements OnInit {
     return await modal.present();
   }
 
+  loadTransaction( event ) {
+    let startDate = moment().utc().subtract(3, 'months').valueOf();
+    let endDate = moment().utc().valueOf();
+    this.fromTx = this.fromTx + 11 ;
+    this.toTx = this.toTx + 10;
+    this.storage.get('wallet').then(async (wallets) => {
+      for (const wallet of wallets) {
+          const tx = await this.heliosService.getAllTransactions( wallet , startDate , endDate, this.fromTx, this.toTx);
+          this.transactions = this.transactions.concat(tx.map( data => {
+            data.timestamp = moment.unix(data.timestamp);
+            return data;
+          }));
+          console.log ( 'getTransactions', this.transactions );
+      }
+      event.target.complete();
+    });
+  }
 }
