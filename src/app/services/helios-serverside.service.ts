eb5d065b7ae2d12743bb1245fd06f93955126889
carlos.molina <sak7258@gmail.com>
2020-04-05 17:03:34 -0400

    init server interaction
    add superagent and bcryptjs
    add all method interaction

---
+++ src/app/services/helios-serverside.service.ts
@@ -0,0 +1,360 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as superagent from 'superagent';
+import bcrypt from 'bcryptjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeliosServersideService {
+
+  serverUrl: string;
+  saltRounds: number;
+  superagent: any;
+  useLocalStorage: boolean;
+  queryResponseTimeout: number;
+  queryResponseDeadline: number;
+  sessionHash: any;
+  username: any;
+
+ constructor() {
+    this.serverUrl = 'http://heliosprotocol.io/wallet-serverside/';
+    this.saltRounds = 11;
+    this.superagent = superagent.agent();
+    this.useLocalStorage = false;
+    this.queryResponseTimeout = 4000; // Time till server responds
+    this.queryResponseDeadline = 8000; // Allowed time for page to load
+  }
+
+saveSession(sessionHash, username) {
+    console.log('Saving session');
+    if (typeof window !== 'undefined' && this.useLocalStorage) {
+        window.localStorage.setItem('sessionHash', sessionHash);
+        if (!(username === undefined)) {
+            window.localStorage.setItem('username', username);
+        }
+    } else {
+        this.sessionHash = sessionHash;
+        if (!(username === undefined)) {
+            this.username = username;
+        }
+    }
+}
+
+loadSession() {
+    if (typeof window !== 'undefined' && this.useLocalStorage) {
+        const sessionHash = window.localStorage.getItem('sessionHash');
+        const username = window.localStorage.getItem('username');
+        return {sessionHash, username};
+    } else {
+        return {sessionHash: this.sessionHash, username: this.username};
+    }
+}
+
+killSession() {
+    if (typeof window !== 'undefined') {
+        window.localStorage.removeItem('sessionHash');
+        window.localStorage.removeItem('username');
+    } else {
+        this.sessionHash = '';
+        this.username = '';
+    }
+}
+
+/**
+ * Renews session
+ * @returns  boolean
+ */
+async renewSession() {
+    console.log('Renewing session');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'renew_session', username: session.username, sessionHash: session.sessionHash};
+      const response = await this.queryServer(query);
+      if (response !== false && 'success' in response) {
+            return true;
+        } else {
+            this.killSession();
+        }
+    }
+    return false;
+}
+
+/**
+ * Gets online wallets
+ * @returns  Object or boolean
+ */
+async getOnlineWallets() {
+    console.log('Getting online wallets');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'get_wallets', username: session.username, sessionHash: session.sessionHash};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Adds online wallet
+ * @param keystore string
+ * @param name string
+ * @returns  Object
+ */
+async addOnlineWallet(keystore, name) {
+    console.log('Adding online wallet');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'add_keystore',
+            username: session.username,
+            sessionHash: session.sessionHash,
+            keystore: JSON.stringify(keystore),
+            wallet_name: name};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Renames online wallet
+ * @param walletId number
+ * @param previousWalletName string
+ * @param newWalletName string
+ * @returns  Object or boolean
+ */
+async renameOnlineWallet(walletId, previousWalletName, newWalletName) {
+    console.log('Renaming online wallet');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'rename_keystore',
+            username: session.username,
+            sessionHash: session.sessionHash,
+            wallet_id: walletId,
+            previous_wallet_name: previousWalletName,
+            new_wallet_name: newWalletName};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Deletes online wallet
+ * @param id number
+ * @param name string
+ * @returns  Object or boolean
+ */
+async deleteOnlineWallet(id, name) {
+    console.log('Deleting online wallet');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'delete_keystore',
+            username: session.username,
+            sessionHash: session.sessionHash,
+            wallet_id: id,
+            wallet_name: name};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Gets contacts
+ * @returns  Object or boolean
+ */
+async getContacts() {
+    console.log('Getting contacts');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'get_contacts', username: session.username, sessionHash: session.sessionHash};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Gets new2 fasecret
+ * @returns  Object or boolean
+ */
+async getNew2FASecret() {
+    console.log('Getting new 2FA secret');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'get_new_2fa_secret', username: session.username, sessionHash: session.sessionHash};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Is2s faenabled
+ * @returns  Object or boolean
+ */
+async is2FAEnabled() {
+    console.log('Checking if 2FA enabled');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'is_2fa_enabled', username: session.username, sessionHash: session.sessionHash};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Delete2s fasecret
+ * @returns  Object or boolean
+ */
+async delete2FASecret() {
+    console.log('Deleting 2FA secret');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'delete_2fa_secret', username: session.username, sessionHash: session.sessionHash};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Save2s fasecret
+ * @param secret string
+ * @param code number
+ * @returns  Object or boolean
+ */
+async save2FASecret(secret, code) {
+    console.log('Saving 2FA secret');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'save_new_2fa_secret', secret, code, username: session.username, sessionHash: session.sessionHash};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Adds contact
+ * @param contactName string
+ * @param contactAddress string
+ * @returns  Object or boolean
+ */
+async addContact(contactName, contactAddress) {
+    console.log('Adding contacts');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'add_contact',
+            username: session.username,
+            sessionHash: session.sessionHash,
+            contact_name: contactName,
+            contact_address: contactAddress};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Deletes contact
+ * @param id number
+ * @returns  Object or boolean
+ */
+async deleteContact(id) {
+    console.log('Deleting contact');
+    const session = this.loadSession();
+    if (!(session.sessionHash === undefined)) {
+      const query = {action: 'delete_contact',
+            username: session.username,
+            sessionHash: session.sessionHash,
+            contact_id: id};
+      return await this.queryServer(query);
+    }
+    return false;
+}
+
+/**
+ * Signs in
+ * @param username string
+ * @param password string
+ * @param tfaCode string
+ * @returns  Object
+ */
+async signIn(username, password, tfaCode) {
+    const query = {action: 'get_salt', username};
+    const saltResponse = await this.queryServer(query);
+    const saltForVerification = saltResponse.salt;
+    const passwordHashForVerification = bcrypt.hashSync(password, saltForVerification);
+
+    const newSalt = bcrypt.genSaltSync(this.saltRounds);
+    const newPasswordHash = bcrypt.hashSync(password, newSalt);
+
+    const querySignIn = {   action: 'sign_in',
+                username,
+                two_factor_code: tfaCode,
+                password_hash_for_verification: passwordHashForVerification,
+                new_password_hash: newPasswordHash,
+                new_salt: newSalt};
+    const response = await this.queryServer(querySignIn);
+    /* if ('sessionHash' in response) {
+        this.saveSession(response.sessionHash, username);
+    } */
+    return response;
+}
+
+/**
+ * News user
+ * @param username string
+ * @param email string
+ * @param password string
+ * @param newWalletKeystore string
+ * @returns  Object
+ */
+async newUser(username, email, password, newWalletKeystore) {
+    const salt = bcrypt.genSaltSync(this.saltRounds);
+    const passwordHash = bcrypt.hashSync(password, salt);
+    const query = {   action: 'new_user',
+                    username,
+                    email,
+                    password_hash: passwordHash,
+                    salt,
+                    new_wallet_keystore: JSON.stringify(newWalletKeystore)};
+    return await this.queryServer(query);
+}
+
+/**
+ * Querys server
+ * @param query string
+ * @returns Object
+ */
+async queryServer(query) {
+    return await superagent.get(this.serverUrl)
+    .timeout({
+        response: this.queryResponseTimeout,
+        deadline: this.queryResponseDeadline,
+    })
+    .query(query)
+    .then(res => {
+        let jsonResponse;
+        try {
+            jsonResponse = JSON.parse(res.text);
+        } catch (e) {
+            throw new Error(`Malformed JSON Response ${res.status}, ${ res.text}`);
+        }
+        console.log('Successful response from server');
+        console.log(jsonResponse);
+        if ('sessionHash' in jsonResponse) {
+            this.saveSession(jsonResponse.sessionHash, null); // TODO sak revisa esto!!
+        }
+        // check for invalid session
+        if ('error' in jsonResponse) {
+            if (jsonResponse.error === 2020) {
+                /* if (typeof window.logout !== 'undefined' && typeof window.popup !== 'undefined') {
+                    console.log('Session expired');
+                    window.logout();
+                    window.popup('Your session has expired. Please log in again to continue.');
+                } */
+            }
+        }
+        return jsonResponse;
+
+    })
+    .catch(err => {
+        throw new Error(`HTTP Request Error ${err.message}, ${ err.response}`);
+    });
+  }
+}
