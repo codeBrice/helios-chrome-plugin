4c249ced0a7deee309bfb1a982e1157316f8e161
Gerardo Montero <monterog24@gmail.com>
2020-04-09 01:36:47 -0400

    Add create user service and tag "a" in home page for create user

--- src/app/pages/wallet/create/create-accounts/create-accounts.page.ts
+++ src/app/pages/wallet/create/create-accounts/create-accounts.page.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { HeliosServersideService } from 'src/app/services/helios-serverside.service';
+import { Storage } from '@ionic/storage';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { HeliosServiceService } from '../../../../services/helios-service.service';
+
 
 
 @Component({
@@ -10,16 +16,43 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class CreateAccountsPage implements OnInit {
 
   createAccountForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+    private heliosServersideService: HeliosServersideService,
+    private storage: Storage,
+    private router: Router,
+    private heliosService: HeliosServiceService,
+    private toastController: ToastController) { }
 
   ngOnInit() {
     //Falta agregar  que las contraseñas coincidan y chequear que el  metodo revise que el usuario no exista en base de datos
     this.createAccountForm = this.formBuilder.group({
       username: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-      repeatPassword: new FormControl('', [Validators.required])
-    });
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(16)]),
+      repeatPassword: new FormControl('', [Validators.required, Validators.minLength(16)])
+    }
+    );
+  }
+
+
+  async createNewAccount() {
+
+    try {
+      //Method create wallet for user
+      const accountWallet = await this.heliosService.accountCreate(this.createAccountForm.value.password);
+      const keystorage = accountWallet.encrypt;
+      const result = await this.heliosServersideService.newUser(this.createAccountForm.value.username, this.createAccountForm.value.email, this.createAccountForm.value.password, keystorage)
+      this.router.navigate(['/homewallet']);
+    } catch (error) {
+      const toast = await this.toastController.create({
+        cssClass: 'text-red',
+        message: error.message,
+        duration: 2000
+      });
+      toast.present();
+
+    }
+
   }
 
 }
