bf4a0101ac46505f48582635369f11e8e7c427da
carlos.molina <sak7258@gmail.com>
2020-04-21 20:08:09 -0400

    code review

--- src/app/pages/wallet/create/generate/generate.page.ts
+++ src/app/pages/wallet/create/generate/generate.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
 import { HeliosServiceService } from '../../../../services/helios-service.service';
 import { Router } from '@angular/router';
 import { LoadingController, ToastController } from '@ionic/angular';
@@ -22,13 +22,13 @@ export class GeneratePage implements OnInit {
   hash: string;
 
   constructor(
-    private formBuilder: FormBuilder,
-    private heliosService: HeliosServiceService,
-    private router: Router,
-    private storage: Storage,
-    private loadingController: LoadingController,
-    public toastController: ToastController,
-    private heliosServersideService: HeliosServersideService
+   private formBuilder: FormBuilder,
+   private heliosService: HeliosServiceService,
+   private router: Router,
+   private storage: Storage,
+   private loadingController: LoadingController,
+   public toastController: ToastController,
+   private heliosServersideService: HeliosServersideService
   ) {
     this.saltRounds = 11;
   }
@@ -41,64 +41,65 @@ export class GeneratePage implements OnInit {
   }
 
   async sendPassword() {
-    const loading = await this.loadingController.create({
-      message: 'Creating wallet...',
-      translucent: true,
-      cssClass: 'custom-class custom-loading'
-    });
-    await loading.present();
-    try {
+      const loading = await this.loadingController.create({
+        message: 'Creating wallet...',
+        translucent: true,
+        cssClass: 'custom-class custom-loading'
+      });
+      await loading.present();
 
-      const accountWallet = await this.heliosService.accountCreate(this.createWallet.value.password);
-      const keystorage = accountWallet.encrypt;
-      const storageUser = await this.storage.get('userInfo');
-      if (storageUser) {
-        await this.heliosServersideService.addOnlineWallet(keystorage, this.createWallet.value.name, storageUser);
-        this.hash = storageUser.sessionHash;
-      } else {
-        this.hash = this.generateHash(this.createWallet.value.password);
-        this.storage.set('userInfoLocal', { sessionHash: this.hash });
-      }
-      sessionStorage.setItem('wallet', accountWallet.account.address);
-      sessionStorage.setItem('privateKey', accountWallet.account.privateKey);
-      sessionStorage.setItem('keystore', JSON.stringify(accountWallet.encrypt));
-      // data storage for mobile
-      this.storage.get('wallet').then(async (wallets) => {
-        try {
-          // const hash = this.generateHash( this.createWallet.value.password );
-          const hash = storageUser.sessionHash;
-          this.storage.set('userInfoLocal', { sessionHash: hash });
-          if (wallets === null) {
-            const walletArray = [new Wallet(accountWallet.account.address,
-              cryptoJs.AES.encrypt(accountWallet.account.privateKey, hash).toString(), this.createWallet.value.name)];
-            this.storage.set('wallet', walletArray);
-          } else {
-            wallets.push(new Wallet(accountWallet.account.address,
-              cryptoJs.AES.encrypt(accountWallet.account.privateKey, hash).toString(), this.createWallet.value.name));
-            this.storage.set('wallet', wallets);
-          }
-          this.router.navigate(['/detailwallet']);
-        } catch (error) {
-          const toast = await this.toastController.create({
-            cssClass: 'text-red',
-            message: error.message,
-            duration: 2000
-          });
-          toast.present();
+      try {
+
+        const accountWallet =  await this.heliosService.accountCreate( this.createWallet.value.password );
+        const keystorage = accountWallet.encrypt;
+        const storageUser = await this.storage.get('userInfo');
+
+        if (storageUser) {
+          await this.heliosServersideService.addOnlineWallet(keystorage, this.createWallet.value.name, storageUser);
+          this.hash = storageUser.sessionHash;
+        } else {
+          this.hash = this.generateHash( this.createWallet.value.password );
+          this.storage.set( 'userInfoLocal', { sessionHash: this.hash } );
         }
-        await loading.dismiss();
-      });
-    } catch (error) {
-      const toast = await this.toastController.create({
-        cssClass: 'text-red',
-        message: error.errorDescription || error.message,
-        duration: 2000
-      });
-      toast.present();
-    }
+
+        sessionStorage.setItem( 'wallet', accountWallet.account.address );
+        sessionStorage.setItem( 'privateKey', accountWallet.account.privateKey );
+        sessionStorage.setItem( 'keystore', JSON.stringify(accountWallet.encrypt) );
+        // data storage for mobile
+        this.storage.get( 'wallet').then(async (wallets) => {
+          try {
+
+           if ( wallets === null) {
+              const walletArray = [new Wallet(accountWallet.account.address,
+                cryptoJs.AES.encrypt( accountWallet.account.privateKey, this.hash ).toString(), this.createWallet.value.name)];
+              this.storage.set( 'wallet', walletArray );
+            } else {
+              wallets.push(new Wallet(accountWallet.account.address,
+                 cryptoJs.AES.encrypt( accountWallet.account.privateKey, this.hash ).toString(), this.createWallet.value.name));
+              this.storage.set( 'wallet', wallets );
+            }
+           this.router.navigate(['/detailwallet']);
+          } catch (error) {
+              const toast = await this.toastController.create({
+                cssClass: 'text-red',
+                message: error.message,
+                duration: 2000
+              });
+              toast.present();
+          }
+          await loading.dismiss();
+        });
+      } catch (error) {
+        const toast = await this.toastController.create({
+          cssClass: 'text-red',
+          message: error.errorDescription || error.message,
+          duration: 2000
+        });
+        toast.present();
+      }
   }
 
-  generateHash(password: any) {
+  generateHash( password: any ) {
     const newSalt = bcrypt.genSaltSync(this.saltRounds);
     const newPasswordHash = bcrypt.hashSync(password, newSalt);
     return newPasswordHash;
